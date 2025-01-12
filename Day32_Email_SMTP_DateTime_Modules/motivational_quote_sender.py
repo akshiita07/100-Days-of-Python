ed6a0107c9607380f1136c79842844b8ba308df2
Akshita Pathak <pathakshita07@gmail.com>
2025-01-12 12:12:01 +0530

    day 32 smtp

---
+++ Day32_Email_SMTP_DateTime_Modules/motivational_quote_sender.py
@@ -0,0 +1,37 @@
+﻿# if it is a particular date & time then email ourself a motivational quote
+import smtplib
+import datetime as dt
+import random
+
+# fetching day:
+current=dt.datetime.now()
+current_day=current.weekday()
+# print(current_day)
+
+
+# FOR .env file: pip install python-dotenv
+from dotenv import load_dotenv
+import os
+load_dotenv("Day32_Email_SMTP_DateTime_Modules/.env")
+# smtp credentials:
+my_email="apathak1_be22@thapar.edu"
+password=os.getenv("EMAIL_PASSWORD")
+print(f"Password fetched by env is: {password}")
+    
+# on a monday:
+if(current_day==1):
+    # select random quote from list of quotes
+    with open("Day32_Email_SMTP_DateTime_Modules/quotes.txt") as quotes_file:
+        quotes_list=quotes_file.readlines()
+        random_quote=random.choice(quotes_list)
+    # print(random_quote) 
+    
+    # send mail
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()       #for secure
+        connection.login(user=my_email,password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="pathakshita07@gmail.com",
+            msg=f"Subject:You got this Akshu!\n\n{random_quote}"
+        )
---
+++ Day32_Email_SMTP_DateTime_Modules/smtp_mails.py
@@ -0,0 +1,38 @@
+﻿# smtplib library to send emails
+# Gmail: smtp.gmail.com     SIMPLE MAIL TRANFER PROTOCOL
+# Python mail app: password-uedr ftsu yiok uoji
+# ADDING PORT NUMEBR: smtplib.SMTP("smtp.gmail.com", port=587)
+import smtplib
+
+# FOR .env file: pip install python-dotenv
+from dotenv import load_dotenv, find_dotenv
+import os
+# dotenv_path = find_dotenv()
+# print(f".env file found at: {dotenv_path}")  # Check if the .env file is detected
+
+load_dotenv("Day32_Email_SMTP_DateTime_Modules/.env")
+
+my_email="apathak1_be22@thapar.edu"
+password=os.getenv("EMAIL_PASSWORD")
+print(f"Password fetched by env is: {password}")
+
+# using close():
+"""
+connection=smtplib.SMTP("smtp.gmail.com")       #to connect to ur gmail
+connection.starttls()       #TLS: TRANSPORT LAYER SECURITY: to secure our connection to email server
+connection.login(user=my_email,password=password)
+connection.sendmail(
+    from_addr=my_email,
+    to_addrs="pathakshita07@gmail.com",
+    msg="Subject:From Python\n\nHi Akshita from python smtp")
+connection.close()
+"""
+
+# without using close():
+with smtplib.SMTP("smtp.gmail.com") as connection:       #to connect to ur gmail
+    connection.starttls()       #TLS: TRANSPORT LAYER SECURITY: to secure our connection to email server
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="pathakshita07@gmail.com",
+        msg="Subject:From Python\n\nHi Akshita from python smtp")
