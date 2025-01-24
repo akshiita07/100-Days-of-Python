06f81e0187345059f3e4ee05f9eb1ebad1f47415
Akshita Pathak <pathakshita07@gmail.com>
2025-01-24 19:58:14 +0530

    Day 45

---
+++ Day45_WebScraping_BeautifulSoup/100_movies_to_watch.py
@@ -0,0 +1,22 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+response=requests.get(url="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+data=response.text
+
+soup=BeautifulSoup(data,"html.parser")
+# print(soup.prettify())
+
+movies=[]
+movie_link=soup.select(".article-title-description__text  .title")
+for m in movie_link:
+    movies.append(m.getText())
+
+print("List of movies:")
+movies.reverse()
+print(movies)
+
+with open("./Day45_WebScraping_BeautifulSoup/moviesList.txt",mode="w",encoding="utf-8") as file:
+    for m in movies:
+        file.write(f"{m}\n")
+    
---
+++ Day45_WebScraping_BeautifulSoup/live_website_scraping.py
@@ -0,0 +1,49 @@
+﻿from bs4 import BeautifulSoup
+import requests
+
+response=requests.get(url="https://news.ycombinator.com/news")
+# print(response.text)  
+
+soup=BeautifulSoup(response.text,"html.parser")
+# print(soup.prettify())
+
+articles=soup.select(".titleline")
+texts=[]
+links=[]
+votes=[]
+
+for item in articles:
+    texts.append(item.find("a").getText())
+    links.append(item.find("a").get("href"))
+    
+upVotes=soup.find_all(name="span",class_="score") 
+for item in upVotes:
+    votes.append(item.getText())
+
+print("\nHeadings:")
+print(texts)
+
+print("\nLinks:")
+print(links)
+
+# print("\nVotes:")
+# print(votes)
+
+# get integer out of votes:
+integer_votes=[]
+for vote in votes:
+    integer_votes.append(int(vote.split()[0]))
+print("\nUpdated Votes:")
+print(integer_votes)
+
+# get maximum upvote count:
+# maxi=integer_votes[0]
+# for i in range(1,len(integer_votes)):
+#     if integer_votes[i]>maxi:
+#         maxi=integer_votes[i]
+#         ind=i
+
+maxi=max(integer_votes)
+ind=integer_votes.index(maxi)
+print(f"Max upvote: {maxi} and index: {ind}")
+print(f"Title of story with maximum upvotes= {texts[ind]} and link is {links[ind]}")
