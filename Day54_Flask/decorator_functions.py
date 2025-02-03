2e1885e213a245498fbccb5515cb96446d18b54f
Akshita Pathak <pathakshita07@gmail.com>
2025-02-03 13:30:10 +0530

    Day 54 flask

---
+++ Day54_Flask/decorator_functions.py
@@ -0,0 +1,32 @@
+﻿
+# UNDERSTANDING DECORATOR FUNCTIONS:
+"""
+def outer():
+    print("I am outer")
+    def inner():
+        print("I am inner")
+    return inner
+    
+fnc=outer()
+fnc()
+"""
+
+import time
+# DECORATOR FUNCTIONS: WRAP ANOTHER FUNCTION & GIVES IT ADDITIONAL FUNCTIONALITY:
+def decorator_function(function):
+    def wrapper_function(*args, **kwargs):
+        # do something BEFORE the function
+        time.sleep(2)
+        function(*args, **kwargs)      #modify this fnc
+        # do something AFTER the function
+    return wrapper_function
+
+@decorator_function
+def say_hello():
+    print("Hi guys")
+    
+def say_bye():
+    print("Bye guys")
+    
+say_hello()
+say_bye()
---
+++ Day54_Flask/hello.py
@@ -0,0 +1,16 @@
+﻿# pip install Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")     #decorator function: ie run hello_world function only when route is /
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def bye_world():
+    return "<p>Goodbye</p>"
+
+# to run file: flask --app hello run
+if __name__=="__main__":
+    app.run()
